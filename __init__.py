37a3a5633cfb35ec7bf929f6b8a97458bba42bb3
dsigmabcn <92378590+dsigmabcn@users.noreply.github.com>
2024-05-13 09:31:07 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,17 @@
 from .civitai_lora_loader import CivitAI_LORA_Loader
 from .civitai_checkpoint_loader import CivitAI_Checkpoint_Loader
+from .civiai_mm_loader import CivitAI_mm_Loader
 
 NODE_CLASS_MAPPINGS = {
     "CivitAI_Lora_Loader": CivitAI_LORA_Loader,
-    "CivitAI_Checkpoint_Loader": CivitAI_Checkpoint_Loader
+    "CivitAI_Checkpoint_Loader": CivitAI_Checkpoint_Loader,
+    "CivitAI_mm_Loader": CivitAI_mm_Loader
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CivitAI_Lora_Loader": "CivitAI Lora Loader",
-    "CivitAI_Checkpoint_Loader": "CivitAI Checkpoint Loader"
+    "CivitAI_Checkpoint_Loader": "CivitAI Checkpoint Loader",
+    "CivitAI_mm_Loader": "CivitAI_mm_Loader"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
